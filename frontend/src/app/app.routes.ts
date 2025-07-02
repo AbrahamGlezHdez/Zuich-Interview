f61a464cee2a48b480b00fa67bacce5240e02252
AbrahamGlezHdez <abgohe@hotmail.com>
2025-07-01 22:59:43 -0600

    feat: Implementacion de guard en Administrador

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -2,11 +2,17 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from './pages/login/login.component';
 import { AdminComponent } from './pages/admin/admin.component';
 import { ClientComponent } from './pages/client/client.component';
+import { authGuard } from './guards/auth.guard';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'login', pathMatch: 'full' },
   { path: 'login', component: LoginComponent },
-  { path: 'admin', component: AdminComponent },
+  {
+    path: 'admin',
+    component: AdminComponent,
+    canActivate: [authGuard],
+    data: { role: 'Administrador' }
+  },
   { path: 'client', component: ClientComponent },
   { path: '**', redirectTo: 'login' }
 ];
